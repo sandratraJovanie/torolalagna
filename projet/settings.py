5118109711d8765b157c20612f06fad2855771a7
Jovanie <rafanomezantsoasandratra@gmail.com>
2022-02-08 19:25:10 +0100

    Update ALLOWED_HOSTS

--- projet/settings.py
+++ projet/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-(lev7yq)f#59ke
 #DEBUG = True
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS =  ['https://torolalagna.herokuapp.com/','127.0.0.1']
 
 
 # Application definition
